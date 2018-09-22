be58aed3dc3ecd74dd38bef6bce9e0afbd67a494
Ayoub <skobraf@gmail.com>
2018-09-22 15:51:43 +0100

    added user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,37 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = users(:valid)
+  end
+
+  test "valid user" do
+  	assert @user.valid?
+  end
+
+  test "invalid without name" do
+  	@user.username = nil
+  	assert_not @user.valid?
+  end
+
+  test "invalid without email" do
+  	@user.email = " "
+  	assert_not @user.valid?
+  end
+
+  test "name should not be too long" do 
+  	@user.username = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "email should not be too long" do 
+  	@user.email = "a" * 244 + "@example.com"
+  	assert_not @user.valid?
+  end
+
+  test "email should be unique" do 
+  	duplicate_user = @user.dup
+  	@user.save
+  	assert_not duplicate_user.valid?
+  end
 end
