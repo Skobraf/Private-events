c99645d52a1ad9dea8b25b8c54caa8c7370f956d
Ayoub <skobraf@gmail.com>
2018-09-22 18:18:43 +0100

    add CRUD for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,43 @@
 class UsersController < ApplicationController
-
+	before_action :set_user, only: [:edit, :update, :destroy]
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:succes] = "Account updated!"
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@user.destroy
+		redirect_to users_path
+	end
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:username, :email)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+	
 end
