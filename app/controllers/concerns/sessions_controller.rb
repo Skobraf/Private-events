631c9988ac0ae3a7b81c90b91d806ea732447694
Ayoub <skobraf@gmail.com>
2018-09-23 21:16:12 +0100

    added create session

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			flash[:success] = 'your logged'
+			redirect_to root_path
+		else
+			flash.now[:danger] = " Email or password are wrong"
+			render 'new'
+		end
 	end
 
 	def destroy
